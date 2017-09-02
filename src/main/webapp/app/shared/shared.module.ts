7b07fac59008dbabf7d66ad426927feaaaa2b5af
Steampunk1453 <dasies11@gmail.com>
2017-09-02 21:48:08 +0200

    make it better

---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,49 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import {
+    RetroAppSharedLibsModule,
+    RetroAppSharedCommonModule,
+    CSRFService,
+    AuthServerProvider,
+    AccountService,
+    UserService,
+    StateStorageService,
+    LoginService,
+    LoginModalService,
+    Principal,
+    HasAnyAuthorityDirective,
+    JhiLoginModalComponent
+} from './';
+
+@NgModule({
+    imports: [
+        RetroAppSharedLibsModule,
+        RetroAppSharedCommonModule
+    ],
+    declarations: [
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective
+    ],
+    providers: [
+        LoginService,
+        LoginModalService,
+        AccountService,
+        StateStorageService,
+        Principal,
+        CSRFService,
+        AuthServerProvider,
+        UserService,
+        DatePipe
+    ],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [
+        RetroAppSharedCommonModule,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective,
+        DatePipe
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class RetroAppSharedModule {}
